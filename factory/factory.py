4da23fe4f4a9c142c0b4d795af10464c932bd74b
Mladen Milosavljevic <Mrakonja@users.noreply.github.com>
2023-04-10 23:49:28 +0200

    first commit

---
+++ factory/factory.py
@@ -0,0 +1,25 @@
+# Example of a factory pattern. Implementation using factory method.
+
+class Button(object):
+   html = ""
+   def get_html(self):
+      return self.html
+
+class Image(Button):
+   html = "<img></img>"
+
+class Input(Button):
+   html = "<input></input>"
+
+class Flash(Button):
+   html = "<obj></obj>"
+
+class ButtonFactory():  #this is actuall factory method pattern
+   def create_button(self, typ):
+      targetclass = typ.capitalize()
+      return globals()[targetclass]()
+
+button_obj = ButtonFactory()
+button = ['image', 'input', 'flash']
+for b in button:
+   print button_obj.create_button(b).get_html()
