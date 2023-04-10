74f57d07a3104f76d9aefca9a1d6952280dae0b1
Mladen Milosavljevic <Mrakonja@users.noreply.github.com>
2023-04-10 23:51:29 +0200

    factory metod update

--- factory/factory.py
+++ factory/factory.py
@@ -22,4 +22,17 @@ class ButtonFactory():  #this is actuall factory method pattern
 button_obj = ButtonFactory()
 button = ['image', 'input', 'flash']
 for b in button:
-   print button_obj.create_button(b).get_html()
+   print(button_obj.create_button(b).get_html())
+
+#The button class helps to create the html tags and the associated html page. The client will not have access to the logic of code and the output represents the creation of html page.
+
+
+#example of practical usecase in real world, is django forms
+
+from django import forms
+
+class PersonForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    birth_date = forms.DateField(required=False)
+
+print(PersonForm)
